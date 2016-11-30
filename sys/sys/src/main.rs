3283925bde427c88e159de360245d2b36659c671
sideffect0 <mail3renjith@gmail.com>
2016-11-30 09:51:03 +0530

    rusting shared borrows

--- sys/sys/src/main.rs
+++ sys/sys/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Pure, Sysafe!");
+
+   loop{
+       check_if_safe(format!("Safe {}", "Borrows"));
+   }
+}
+
+
+fn check_if_safe(s_borrow: String){
+
+     println!("Pure, {}!", s_borrow); 
 }
